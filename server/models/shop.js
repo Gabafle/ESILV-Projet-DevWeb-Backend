d9c070aef2165e7288faab637b5893f89449eec5
gabri <gabriel.peter@edu.devinci.fr>
2024-12-19 15:57:31 +0100

    modify shop models

--- server/models/shop.js
+++ server/models/shop.js
@@ -1,37 +1,34 @@
-import connection from "./db";
+const {DataTypes} = require('sequelize');
+const sequelize = require('../db/connection');
+const user = require('../models/user');
 
-const users = connection.db("users");
-
-class shope extends Model {}
-
-Shop.init({
+const Shop = sequelize.define("Shop", {
     name: {
-        TYPE : String,
-        defaultValue : " NewShop",
-        allowNull : false,
+        TYPE: String,
+        defaultValue: "NewShop",
+        allowNull: false,
     },
     description: {
-        TYPE : String,
-        defaultValue : "",
+        TYPE: String,
+        defaultValue: "",
     },
-    address: {
-        TYPE : String,
-        defaultValue : "Sweet Home Alabama",
+    address: { TYPE: String,
+        defaultValue: "Sweet Home Alabama",
     },
-    avis:{
-        TYPE : String,
-        defaultValue : "",
+    avis: {
+        TYPE: String,
+        defaultValue: "",
     },
-    note:{
-        TYPE : Number ,
-        defaultValue : 0
+    note: {
+        TYPE: Number,
+        defaultValue: 0,
     },
-    articles:{
-        TYPE : []
+    articles: {
+        TYPE: Array,
+        defaultValue: [],
     },
-    owner:{
-        TYPE : users
+    owner: {
+        TYPE: user,
+        defaultValue: null,
     },
-
-}
-)
+})
