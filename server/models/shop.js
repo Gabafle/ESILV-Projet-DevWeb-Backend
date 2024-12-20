45c85a3ddac8168ada289ba2e4d8a63173ca2668
gabri <gabriel.peter@edu.devinci.fr>
2024-12-20 16:57:52 +0100

    MISS shop

--- server/models/shop.js
+++ server/models/shop.js
@@ -1,7 +1,7 @@
 const {DataTypes} = require('sequelize');
 const sequelize = require('./db');
 const user = require('../models/user');
-const connection = require("./db");
+
 
 const Shop = sequelize.define("Shop", {
     name: {
