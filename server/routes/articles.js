77e45ee4de5b764a9a09efa7c9592c42b9a04b70
mickTa <157215534+mickTa@users.noreply.github.com>
2024-12-21 15:24:55 +0100

    Use articles with controller

--- server/routes/articles.js
+++ server/routes/articles.js
@@ -1,96 +1,14 @@
-const express = require('express');
-const checkAuth = require('../middlewares/checkAuth');
-const Article = require('../models/articles');
+const { Router } = require("express");
+const ArticleController = require("../controllers/articles");
+const checkAuth = require("../middlewares/checkAuth");
+const checkAdmin = require("../middlewares/checkAdmin");
 
-const router = express.Router();
+const router = new Router();
 
-//Récupérer tous les articles
-router.get('/', async (req, res) => {
-  try {
-    const articles = await Article.findAll();
-    res.json(articles);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to fetch articles.' });
-  }
-});
-
-//Récupérer un article
-router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const article = await Article.findByPk(id);
-    if (!article) {
-      return res.status(404).json({ error: 'Article not found' });
-    }
-    res.json(article);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to fetch article.' });
-  }
-});
-
-//Nouveau Articles
-router.post('/', checkAuth, async (req, res) => {
-  const { title, content, price, quantity } = req.body;
-  const authorId = req.user.id; // Récupéré du token JWT
-
-  try {
-    const article = await Article.create({ title, content, price, quantity, authorId });
-    res.status(201).json(article);
-  } catch (error) {
-    console.error(error);
-    res.status(400).json({ error: 'Failed to create article.' });
-  }
-});
-
-//MAJ Article
-router.patch('/:id', checkAuth, async (req, res) => {
-  const { id } = req.params;
-  const { title, content, price, quantity } = req.body;
-
-  try {
-    const article = await Article.findByPk(id);
-
-    if (!article) {
-      return res.status(404).json({ error: 'Article not found' });
-    }
-
-    // Vérifier si l'utilisateur est l'auteur
-    if (article.authorId !== req.user.id) {
-      return res.status(403).json({ error: 'Unauthorized' });
-    }
-
-    await article.update({ title, content, price, quantity });
-    res.json(article);
-  } catch (error) {
-    console.error(error);
-    res.status(400).json({ error: 'Failed to update article. Check your input.' });
-  }
-});
-
-router.delete('/:id', checkAuth, async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const article = await Article.findByPk(id);
-
-    if (!article) {
-      return res.status(404).json({ error: 'Article not found' });
-    }
-
-    // Vérifier si l'utilisateur est l'auteur
-    if (article.authorId !== req.user.id) {
-      return res.status(403).json({ error: 'Unauthorized' });
-    }
-
-    await article.destroy();
-    res.status(204).send();
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to delete article.' });
-  }
-});
+router.get("/", ArticleController.cget);
+router.get("/:id", ArticleController.get);
+router.post("/", checkAuth, checkAdmin, ArticleController.post);
+router.patch("/:id", checkAuth, checkAdmin, ArticleController.patch);
+router.delete("/:id", checkAuth, checkAdmin, ArticleController.delete);
 
 module.exports = router;
