8c50dffdecf31afcc19872636462b528bcd3cc44
mickTa <157215534+mickTa@users.noreply.github.com>
2024-12-21 15:53:32 +0100

    Add checkOwnershipOrAdmin for users

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,14 +1,16 @@
-const {Router} = require("express");
+const { Router } = require("express");
 const UserController = require("../controllers/users");
 const checkAuth = require("../middlewares/checkAuth");
+const checkAdmin = require("../middlewares/checkAdmin");
+const checkOwnershipOrAdmin = require("../middlewares/checkOwnershipOrAdmin");
 
 const router = new Router();
 
-router.get("/users", checkAuth, UserController.cget);
+router.get("/users", checkAuth, checkAdmin, UserController.cget);
 router.post("/users", UserController.post);
 
-router.get("/users/:id", UserController.get);
-router.patch("/users/:id", UserController.patch);
-router.delete("/users/:id", checkAuth, UserController.delete);
+router.get("/users/:id", checkAuth, checkOwnershipOrAdmin, UserController.get);
+router.patch("/users/:id", checkAuth, checkOwnershipOrAdmin, UserController.patch);
+router.delete("/users/:id", checkAuth, checkOwnershipOrAdmin, UserController.delete);
 
 module.exports = router;
