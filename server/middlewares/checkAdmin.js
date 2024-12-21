d1f333cabc60fd5e5f7e8d693aa97377857b65c0
mickTa <157215534+mickTa@users.noreply.github.com>
2024-12-21 15:25:24 +0100

    Add files via upload

---
+++ server/middlewares/checkAdmin.js
@@ -0,0 +1,7 @@
+module.exports = async (req, res, next) => {
+    if (!req.user || req.user.role !== "ADMIN") {
+      return res.status(403).json({ error: "Admins only." });
+    }
+    next();
+  };
+  
